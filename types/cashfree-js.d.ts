0f62fe771e4073772ddc04cf0abdebf761df6a36
Adnan <adnanali23189@gmail.com>
2025-09-11 04:58:30 +0530

    Add types to type folder with new env type cashfreejs

---
+++ types/cashfree-js.d.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '@cashfreepayments/cashfree-js' {
+  export function load(options: { mode: 'sandbox' | 'production' }): Promise<any>;
+}
